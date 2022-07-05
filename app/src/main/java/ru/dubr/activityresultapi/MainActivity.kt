3b7f732798b343053a038a25e856b8c4f1d28211
Dubravin Evgeny <dubrdubr23@gmail.com>
2022-07-05 18:58:31 +0300

    Stock permission contract

--- app/src/main/java/ru/dubr/activityresultapi/MainActivity.kt
+++ app/src/main/java/ru/dubr/activityresultapi/MainActivity.kt
@@ -1,11 +1,33 @@
 package ru.dubr.activityresultapi
 
+import android.Manifest.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.*
+import ru.dubr.activityresultapi.databinding.ActivityEditTextBinding
+import ru.dubr.activityresultapi.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
+        Log.d(MainActivity::class.java.simpleName, "Permission result: $it")
+    }
+
+    //private val editMessageLauncher = registerForActivityResult()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        binding.permissionButton.setOnClickListener { requestPermission() }
+    }
+
+    private fun requestPermission() {
+        requestPermissionLauncher.launch(permission.ACCESS_FINE_LOCATION)
     }
 }
